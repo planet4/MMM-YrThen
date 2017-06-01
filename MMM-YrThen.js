89b7c0baef40efc070f86ef787dafa91bcb80d00
Fredrik Mandal <fmandal@gmail.com>
2017-06-01 23:37:59 +0200

    Removed nowCast

--- MMM-YrThen.js
+++ MMM-YrThen.js
@@ -1,8 +1,8 @@
 Module.register('MMM-YrThen', {
-	defaults: {
+    defaults: {
         yrApiUrl: "https://www.yr.no/api/v0/locations/id/%s/forecast",
         updateInterval: 10000
-	},
+    },
 
     getTranslations: function() {
         return {
@@ -23,9 +23,9 @@ Module.register('MMM-YrThen', {
         ];
     },
 
-	start: function() {
-		this.list = null;
-		this.loaded = false;
+    start: function() {
+        this.list = null;
+        this.loaded = false;
         var forecastUrl = printf(printf('%s', this.config.yrApiUrl),this.config.locationId);
         this.getForecast(forecastUrl);
         var self = this;
@@ -33,18 +33,14 @@ Module.register('MMM-YrThen', {
         setInterval(function() {
             self.updateDom(1000);
         }, 60000);
-	},
+    },
 
     socketNotificationReceived: function(notification, payload) {
-		if(notification === 'YR_FORECAST_DATA') {
-			if(payload.nowcast.points != null) {
-                this.processNowcast(payload.nowcast);
-                if(this.config.showWeatherForecast)
-                    this.processForecast(payload.forecast);
-            }
+        if(notification === 'YR_FORECAST_DATA') {
+            this.processForecast(payload.forecast);
             this.updateDom(1000);
-		}
-	},
+        }
+    },
 
     getForecast: function(url) {
         this.sendSocketNotification('GET_YR_FORECAST', {
@@ -53,91 +49,24 @@ Module.register('MMM-YrThen', {
         });
     },
 
-    getNextPrecipStart: function() {
-        return this.list.points.filter((item) => 
-            item.precipitation.intensity > 0 && Date.parse(item.time) >= new Date().valueOf())[0];
-    },
-
-    getNextPrecipStop: function() {
-        return this.list.points.filter((item) => 
-            item.precipitation.intensity === 0 && Date.parse(item.time) >= new Date().valueOf())[0];
-    },
-
-    getMinutesTill: function(nextItemTime) {
-        return Math.abs(Date.parse(nextItemTime) - new Date().valueOf()) / (1000 * 60);
-    },
-
-	getDom: function() {
-		var wrapper = document.createElement('div');
+    getDom: function() {
+        var wrapper = document.createElement('div');
         var animationWrapper = document.createElement('div');
         animationWrapper.className = 'animation';
 
-		if (!this.loaded) {
-			wrapper.innerHTML = this.translate('loading');
-			wrapper.className = 'dimmed light small';
-			return wrapper;
-	    }
-        var nowCast = this.translate('no_precip_next_90');
-        var precipitationStart = this.getNextPrecipStart();
-        var precipitationStop = this.getNextPrecipStop();
+        if (!this.loaded) {
+            wrapper.innerHTML = this.translate('loading');
+            wrapper.className = 'dimmed light small';
+            return wrapper;
+        }
         var forecast = document.createElement('div');
         forecast.className = 'forecast';
 
-        if(precipitationStart != null) {
-            //Precip some time during the next 90 minutes
-            var precipitationStartsIn = this.getMinutesTill(precipitationStart.time);
-            forecast.appendChild(animationWrapper);
-                
-            //Precip now
-            if(precipitationStartsIn < 7) {
-                this.createAnimation(animationWrapper);
-                forecast.appendChild(this.getUmbrella());            
-                if(precipitationStop) {
-                    precipitationStopsIn = this.getMinutesTill(precipitationStop.time);
-                    nowCast = printf(this.translate("precipitation_ends"), precipitationStopsIn.toFixed(0));
-                }
-                else
-                    nowCast = this.translate("precip_next_90");
-            }
-            else {
-                //Precip in n minutes
-                forecast.appendChild(this.getUmbrella());
-                nowCast = printf(this.translate("precip_in"), precipitationStartsIn.toFixed(0));
-            }
-        }
-
-        if(nowCast == this.translate('no_precip_next_90') && this.config.showWeatherForecast) {
-            forecast.appendChild(this.getWeatherSymbol());
-        }
+        forecast.appendChild(this.getWeatherSymbol());
         wrapper.appendChild(forecast);
         wrapper.appendChild(this.getTemperature());
         wrapper.appendChild(this.createNowcastText(nowCast));
-    	return wrapper;
-	},
-
-    createNowcastText: function(nowCast) {
-        var nowCastText = document.createElement('p');   
-        nowCastText.className = 'medium precipText';
-        nowCastText.innerHTML = nowCast;
-        return nowCastText
-    }, 
-
-    createAnimation: function(testElement) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                testElement.appendChild(xhr.responseXML.documentElement);
-            }
-        };
-        xhr.open('GET', this.file('images/rain.svg'), true);
-        xhr.send('');
-    },
-
-    getUmbrella: function() {
-        var umbrella = document.createElement('img');
-        umbrella.className = 'umbrella';
-        umbrella.src = this.file('images/umbrella.svg');
-        return umbrella;
+        return wrapper;
     },
 
     getWeatherSymbol: function() {
@@ -153,13 +82,6 @@ Module.register('MMM-YrThen', {
         temp.innerHTML = printf('%s°', Math.round(this.temperature));
         return temp;
     },
-
-	processNowcast: function(obj) {
-        if(obj.points) {
-            this.list = obj;
-            this.loaded = true;	
-        }
-	},
 
     calculateWeatherSymbolId: function(data) {
         if (!data) return '';
