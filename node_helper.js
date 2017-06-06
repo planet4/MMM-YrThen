f728331708bc21990cf44e1635a47885c8c10249
Fredrik Mandal <fmandal@gmail.com>
2017-06-06 03:41:34 +0200

    Added date and time to console logging
    Using it to check update interval in console log.
    Reminder to self: tail -f ~/.pm2/logs/MagicMirror-out-0.log

--- node_helper.js
+++ node_helper.js
@@ -1,18 +1,24 @@
 var NodeHelper = require('node_helper');
 var request = require('request');
 
-console.log("MMM-YrThen: Starting MMM-YrThen in node_helper");
+var d = new Date();
+var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+console.log("MMM-YrThen " + n + ": Starting MMM-YrThen in node_helper");
 
 module.exports = NodeHelper.create({
     start: function() {
-        console.log("MMM-YrThen: Starting helper for MMM-YrThen");
+        var d = new Date();
+        var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+        console.log("MMM-YrThen " + n + ": Starting helper for MMM-YrThen");
         this.config = null;
         this.forecastUrl = '';
     },
 
     socketNotificationReceived: function(notification, payload) {
         var self = this;
-        console.log("MMM-YrThen: Received notification");
+        var d = new Date();
+        var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+        console.log("MMM-YrThen " + n + ": Received notification");
         if(notification === 'GET_YRTHEN_FORECAST') {
             console.log("MMM-YrThen: Received GET_YRTHEN_FORECAST notification");
             self.config = payload.config;
@@ -22,20 +28,27 @@ module.exports = NodeHelper.create({
     },
 
     getForecastFromYrThen: function() {
-        console.log("MMM-YrThen: Getting forecast");
+        var d = new Date();
+        var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+        console.log("MMM-YrThen " + n + ": Getting forecast");
         var self = this;
         var locationData = {};
 
         request({url: self.forecastUrl, method: 'GET'}, function(error, response, message) {
-            console.log("MMM-YrThen: Requesting forecast from" + self.forecastUrl);
+            var d = new Date();
+            var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+            console.log("MMM-YrThen " + n + ": Requesting forecast from" + self.forecastUrl);
             if (!error && (response.statusCode == 200 || response.statusCode == 304)) {
                 locationData.forecast = JSON.parse(message);
-                console.log("MMM-YrThen: Returning forecast");
-                console.log("MMM-YrThen: Forecast data:" + locationData);
+                var d = new Date();
+                var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+                console.log("MMM-YrThen " + n + ": Returning forecast");
                 self.sendSocketNotification('YRTHEN_FORECAST_DATA', locationData);
             }
             else{
-                console.log("MMM-YrThen: Error!");
+                var d = new Date();
+                var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+                console.log("MMM-YrThen " + n + ": Error!");
             }
         });
     }
