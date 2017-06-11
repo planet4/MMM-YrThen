3b2cebc6c4923191aa45f0f998b5eb9732138854
Fredrik Mandal <fmandal@gmail.com>
2017-06-11 02:01:11 +0200

    Added detailed forecast option.

--- MMM-YrThen.js
+++ MMM-YrThen.js
@@ -5,6 +5,9 @@ Module.register('MMM-YrThen', {
         yrCelestialApiUrl: "https://www.yr.no//api/v0/locations/%s/celestialevent",
         updateInterval: 3600000,
         initialLoadDelay: 1000,
+        showAll: true,
+        showPrecipitation: true,
+        showMaxMin: true,
         details: true,
         numDetails: 2,
         title: 'Værmelding for Steinkjer',
@@ -62,47 +65,113 @@ Module.register('MMM-YrThen', {
 
         var table = document.createElement('table');
         table.className = "xsmall yrthen yrThenForecast";
+        var day;
 
-        for (var f in this.dataFromYr) {
-            var newData = this.dataFromYr[f];
-            var checkTime = moment(newData.start).format("HH");
+        if(this.config.showAll == true){
+            var timeRow = document.createElement('tr');
+            table.appendChild(timeRow);
+            for(var i = 0; i < 5; i++){
+                var newCell = document.createElement('td');
+                newCell.className = 'yrthen-time-cell align-center bright small';
+                if(i == 0) newCell.innerHTML = '&nbsp;';
+                if(i == 1) newCell.innerHTML = '00';
+                if(i == 2) newCell.innerHTML = '06';
+                if(i == 3) newCell.innerHTML = '12';
+                if(i == 4) newCell.innerHTML = '18';
+                timeRow.appendChild(newCell);
+            }
 
-            var show = false;
-            if(f < this.config.numDetails && this.config.details == true) show = true;
-            if(checkTime > 11 && checkTime < 15) show = true;
-            if(show == true){
-                var row = document.createElement('tr');
-                table.appendChild(row);
 
-                var dayCell = document.createElement("td");
-                dayCell.className = "yrthen-day align-left";
-                if(f < this.config.numDetails && this.config.details == true) dayCell.innerHTML = moment(newData.start).format("ddd HH:mm");
-                else dayCell.innerHTML = moment(newData.start).format("dddd");
-                row.appendChild(dayCell);
+            for (var f in this.dataFromYr) {
+                var newData = this.dataFromYr[f];
+                var checkTime = moment(newData.start).format("HH");
+                var today = moment(newData.start).format("ddd");
+                // Make max four cells pr day
+                if(day != today){
+                    var row = document.createElement('tr');
+                    table.appendChild(row);
 
-                var iconCell = document.createElement("td");
-                iconCell.className = "yrthen-icon-cell";
-                row.appendChild(iconCell);
+                    var dayCell = document.createElement("td");
+                    dayCell.className = "yrthen-day align-left";
+                    dayCell.innerHTML = moment(newData.start).format("dddd");
+                    row.appendChild(dayCell);
+                    day = today;
+                }
 
+                var forecastCell = document.createElement("td");
+                forecastCell.className = "yrthen-forecast-cell";
+//                forecastCell.innerHTML = '<span class="dimmed">' + moment(newData.start).format("HH") + '</span><br>';
                 var icon = document.createElement("img");
                 icon.className = "yrthen-icon ";
                 icon.width = "40";
                 var weatherSymbol = this.calculateWeatherSymbolId(newData.symbol);
                 icon.src = this.file(printf('images/%s.svg', weatherSymbol));
-                iconCell.appendChild(icon);
-    
-                var maxTempCell = document.createElement("td");
-                maxTempCell.innerHTML = newData.temperature.value;
-                maxTempCell.className = "align-right bright yrthen-temp small";
-                row.appendChild(maxTempCell);
+                forecastCell.appendChild(icon);
+                if(!this.config.showMaxMin){
+                    forecastCell.innerHTML += '<br>';
+                }
+                forecastCell.innerHTML += ' <span class="bright yrthen-temp small">' + newData.temperature.value + '</span>';
+                if(this.config.showMaxMin){
+                    forecastCell.innerHTML += '<br>';
+                }
 
-                var minTempCell = document.createElement("td");
-                minTempCell.innerHTML = newData.precipitation.value;
-                minTempCell.className = "align-right yrthen-rain dimmed";
-                row.appendChild(minTempCell);
+                if(newData.temperature.min && newData.temperature.max && this.config.showMaxMin){
+                    forecastCell.innerHTML += '<span class="dimmed yrthen-temp">(' + newData.temperature.max + '/' + newData.temperature.min + ')</span><br>';
+                }
+                if(this.config.showPrecipitation){
+                    var precValue = ' <span class="yrthen-rain dimmed">(' + newData.precipitation.value;
+                    if(this.config.showMaxMin){
+                        precValue += ' mm';
+                    }
+                    precValue += ')</span>';
+                    forecastCell.innerHTML += precValue;
+                }
+                row.appendChild(forecastCell);
             }
-
         }
+
+        else{
+            for (var f in this.dataFromYr) {
+                var newData = this.dataFromYr[f];
+                var checkTime = moment(newData.start).format("HH");
+
+                var show = false;
+                if(f < this.config.numDetails && this.config.details == true) show = true;
+                if(checkTime > 11 && checkTime < 15) show = true;
+                if(show == true){
+                    var row = document.createElement('tr');
+                    table.appendChild(row);
+
+                    var dayCell = document.createElement("td");
+                    dayCell.className = "yrthen-day align-left";
+                    if(f < this.config.numDetails && this.config.details == true) dayCell.innerHTML = moment(newData.start).format("ddd HH:mm");
+                    else dayCell.innerHTML = moment(newData.start).format("dddd");
+                    row.appendChild(dayCell);
+
+                    var iconCell = document.createElement("td");
+                    iconCell.className = "yrthen-icon-cell";
+                    row.appendChild(iconCell);
+
+                    var icon = document.createElement("img");
+                    icon.className = "yrthen-icon ";
+                    icon.width = "40";
+                    var weatherSymbol = this.calculateWeatherSymbolId(newData.symbol);
+                    icon.src = this.file(printf('images/%s.svg', weatherSymbol));
+                    iconCell.appendChild(icon);
+        
+                    var maxTempCell = document.createElement("td");
+                    maxTempCell.innerHTML = newData.temperature.value;
+                    maxTempCell.className = "align-right bright yrthen-temp small";
+                    row.appendChild(maxTempCell);
+
+                    var minTempCell = document.createElement("td");
+                    minTempCell.innerHTML = newData.precipitation.value;
+                    minTempCell.className = "align-right yrthen-rain dimmed";
+                    row.appendChild(minTempCell);
+                }
+            }
+        }
+
 
         wrapper.appendChild(table);
         return wrapper;
